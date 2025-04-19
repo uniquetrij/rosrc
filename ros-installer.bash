e24c540c881fe44cf433f16f7814f1aad7922b87
Trijeet Modak <uniquetrij@gmail.com>
2025-04-19 09:05:23 +0530

    Remove echo statement for downloading ~/.rosrc and update installer execution command

--- ros-installer.bash
+++ ros-installer.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo "Downloading ~/.rosrc"
-
+# Download ~/.rosrc
 if [[ -f ~/.rosrc ]]; then mv ~/.rosrc ~/.rosrc.stale.$(date +%Y%m%d%H%M%S.%s); fi
 curl -s https://raw.githubusercontent.com/uniquetrij/rosrc/refs/heads/main/rosrc > ~/.rosrc
 # Source it into your ~/.bashrc if not already done
@@ -9,4 +8,4 @@ grep -q "source ~/.rosrc" ~/.bashrc || echo "source ~/.rosrc" >> ~/.bashrc
 # Refresh your bash
 source ~/.bashrc
 # Execute the installer
-bash rosinstall
+rosinstall
