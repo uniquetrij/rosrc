be81986e556030b21a735d7d93201c25d6f69dad
Trijeet Modak <uniquetrij@gmail.com>
2025-04-19 09:51:10 +0530

    Update ros-installer to source new ~/.rosrc and execute installer

--- ros-installer.bash
+++ ros-installer.bash
@@ -3,7 +3,9 @@
 # Download ~/.rosrc
 if [[ -f ~/.rosrc ]]; then mv ~/.rosrc ~/.rosrc.stale.$(date +%Y%m%d%H%M%S.%s); fi
 curl -s https://raw.githubusercontent.com/uniquetrij/rosrc/refs/heads/main/rosrc > ~/.rosrc
-# Source it into your ~/.bashrc if not already done
+# Source the new ~/.rosrc
+source ~/.rosrc
+# Execute the installer
+rosinstall
+# Permanently add the source command to ~/.bashrc
 grep -q "source ~/.rosrc" ~/.bashrc || echo "source ~/.rosrc" >> ~/.bashrc
-# Refresh your bash
-source ~/.bashrc
