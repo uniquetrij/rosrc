c65e817cb4273b67695145042a65b642d7327a03
Trijeet Modak <uniquetrij@gmail.com>
2025-04-19 08:44:53 +0530

    Change shebang to /bin/sh and update conditional syntax for compatibility

--- ros-installer.sh
+++ ros-installer.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 # Download the latest ~/.rosrc
-if [[ -f ~/.rosrc ]]; then mv ~/.rosrc ~/.rosrc.stale.$(date +%Y%m%d%H%M%S.%s); fi
+if [ -f ~/.rosrc ]; then mv ~/.rosrc ~/.rosrc.stale.$(date +%Y%m%d%H%M%S.%s); fi
 curl -s https://raw.githubusercontent.com/uniquetrij/rosrc/refs/heads/main/rosrc > ~/.rosrc
 # Source it into your ~/.bashrc if not already done
 grep -q "source ~/.rosrc" ~/.bashrc || echo "source ~/.rosrc" >> ~/.bashrc
