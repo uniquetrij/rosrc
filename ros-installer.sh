4a1af4f189f0150d06c5021ccc96041aaf7542fb
Trijeet Modak <uniquetrij@gmail.com>
2025-04-19 12:45:56 +0530

    Reorganize ros-installer.sh to ensure curl is installed before executing further commands

--- ros-installer.sh
+++ ros-installer.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+# Ensure curl is installed
+sudo apt update && sudo apt install --reinstall -y curl
 if [ -f ~/.rosrc ]; then mv ~/.rosrc ~/.rosrc.stale.$(date +%Y%m%d%H%M%S.%s); fi
 curl -s https://raw.githubusercontent.com/uniquetrij/rosrc/refs/heads/main/rosrc > ~/.rosrc
 grep -q "source ~/.rosrc" ~/.bashrc || echo "source ~/.rosrc" >> ~/.bashrc
