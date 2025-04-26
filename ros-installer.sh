c6d461da75a9bd94750412fad87a1d548a90d1d1
Trijeet Modak <uniquetrij@gmail.com>
2025-04-26 21:07:21 +0530

    Refactor environment variable usage in installer scripts for consistency and clarity

--- ros-installer.sh
+++ ros-installer.sh
@@ -15,13 +15,13 @@ if [ -f ~/.rosrc ]; then
 fi
 
 # Download the latest version of the rosrc file from the GitHub repository
-curl -s https://raw.githubusercontent.com/uniquetrij/rosrc/refs/heads/${branch:-main}/rosrc >~/.rosrc
+curl -s https://raw.githubusercontent.com/uniquetrij/rosrc/refs/heads/${ROSRC_GIT_BRANCH:-main}/rosrc >~/.rosrc
 
 # Add a command to source the .rosrc file in .bashrc if it is not already present
 grep -q "source ~/.rosrc" ~/.bashrc || echo "source ~/.rosrc" >>~/.bashrc
 
 # Source the .rosrc file and run the rosinstall command
-bash -c "source ~/.rosrc && rosinstall"
+bash -c "source ~/.rosrc && $ROS_DISTRO=rolling rosinstall"
 
 # Download and execute the extrc-installer.sh script from the GitHub repository
 curl -s https://raw.githubusercontent.com/uniquetrij/bashrc-extensions/refs/heads/main/extrc-installer.sh | sh
